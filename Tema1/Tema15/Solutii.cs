b8ea7b2f2acf9a037c4378b9b7ff1ef09eaacefa
sandrugabriel <sandru11.gabriel@gmail.com>
2024-07-14 17:04:33 +0300

    done

---
+++ Tema1/Tema15/Solutii.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema15
+{
+    public class Solutii
+    {
+
+        Functii functii = new Functii();
+
+        /*At a lemonade stand, each lemonade costs $5. Customers are standing in a queue to buy from you and order one at a time (in the order specified by bills). Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill. You must provide the correct change to each customer so that the net transaction is that the customer pays $5.
+
+Note that you do not have any change in hand at first.
+
+Given an integer array bills where bills[i] is the bill the ith customer pays, return true if you can provide every customer with the correct change, or false otherwise.
+
+
+
+Example 1:
+
+Input: bills = [5,5,5,10,20]
+Output: true
+Explanation: 
+From the first 3 customers, we collect three $5 bills in order.
+From the fourth customer, we collect a $10 bill and give back a $5.
+From the fifth customer, we give a $10 bill and a $5 bill.
+Since all customers got correct change, we output true.
+Example 2:
+
+Input: bills = [5,5,10,10,20]
+Output: false
+Explanation: 
+From the first two customers in order, we collect two $5 bills.
+For the next two customers in order, we collect a $10 bill and give back a $5 bill.
+For the last customer, we can not give the change of $15 back because we only have two $10 bills.
+Since not every customer received the correct change, the answer is false.
+*/
+        public void solutia1()
+        {
+
+            int[] bills1 = { 5, 5, 5, 10, 20 };
+            Console.WriteLine(functii.LemonadeChange(bills1));
+        }
+        
+        /*Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+
+
+
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]*/
+        public void solutia2()
+        {
+
+            int[][] matrix1 = new int[][]
+            {
+            new int[] { 1, 2, 3 },
+            new int[] { 4, 5, 6 },
+            new int[] { 7, 8, 9 }
+            };
+
+            int[][] result1 = functii.Transpose(matrix1);
+            PrintMatrix(result1);
+        }
+        public static void PrintMatrix(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        /*Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary representation of n. If there are no two adjacent 1's, return 0.
+
+Two 1's are adjacent if there are only 0's separating them (possibly no 0's). The distance between two 1's is the absolute difference between their bit positions. For example, the two 1's in "1001" have a distance of 3.
+
+
+
+Example 1:
+
+Input: n = 22
+Output: 2
+Explanation: 22 in binary is "10110".
+The first adjacent pair of 1's is "10110" with a distance of 2.
+The second adjacent pair of 1's is "10110" with a distance of 1.
+The answer is the largest of these two distances, which is 2.
+Note that "10110" is not a valid pair since there is a 1 separating the two 1's underlined.
+Example 2:
+
+Input: n = 8
+Output: 0
+Explanation: 8 in binary is "1000".
+There are not any adjacent pairs of 1's in the binary representation of 8, so we return 0.
+Example 3:
+
+Input: n = 5
+Output: 2
+Explanation: 5 in binary is "101".
+*/
+        public void solutia3()
+        {
+
+            int n1 = 22;
+            int result1 = functii.BinaryGap(n1);
+            Console.WriteLine(n1+" "+result1);
+        }
+
+        /*Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+
+
+Example 1:
+
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+Example 2:
+
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false*/
+        public void solutia4()
+        {
+
+            TreeNode root3 = new TreeNode(1);
+            root3.left = new TreeNode(2);
+            root3.right = new TreeNode(3);
+
+            TreeNode root4 = new TreeNode(1);
+            root4.left = new TreeNode(3);
+            root4.right = new TreeNode(2);
+
+            bool result2 = functii.LeafSimilar(root3, root4);
+            Console.WriteLine(result2);
+        }
+
+        /*Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+*/
+        public void solutia5()
+        {
+            ListNode head1 = new ListNode(1);
+            head1.next = new ListNode(2);
+            head1.next.next = new ListNode(3);
+            head1.next.next.next = new ListNode(4);
+            head1.next.next.next.next = new ListNode(5);
+
+            ListNode middle1 = functii.MiddleNode(head1);
+            PrintList(middle1);
+        }
+        private static void PrintList(ListNode node)
+        {
+            while (node != null)
+            {
+                Console.Write(node.val + " -> ");
+                node = node.next;
+            }
+            Console.WriteLine("null");
+        }
+
+        /*You are given an n x n grid where we place some 1 x 1 x 1 cubes that are axis-aligned with the x, y, and z axes.
+
+Each value v = grid[i][j] represents a tower of v cubes placed on top of the cell (i, j).
+
+We view the projection of these cubes onto the xy, yz, and zx planes.
+
+A projection is like a shadow, that maps our 3-dimensional figure to a 2-dimensional plane. We are viewing the "shadow" when looking at the cubes from the top, the front, and the side.
+
+Return the total area of all three projections.
+
+
+
+Example 1:
+
+
+Input: grid = [[1,2],[3,4]]
+Output: 17
+Explanation: Here are the three projections ("shadows") of the shape made with each axis-aligned plane.
+Example 2:
+
+Input: grid = [[2]]
+Output: 5
+Example 3:
+
+Input: grid = [[1,0],[0,2]]
+Output: 8
+*/
+        public void solutia6()
+        {
+
+            int[][] grid1 = new int[][] {
+            new int[] {1, 2},
+            new int[] {3, 4}
+             };
+            Console.WriteLine(functii.ProjectionArea(grid1));
+        }
+
+        /*A sentence is a string of single-space separated words where each word consists only of lowercase letters.
+
+A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+
+Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: s1 = "this apple is sweet", s2 = "this apple is sour"
+Output: ["sweet","sour"]
+Example 2:
+
+Input: s1 = "apple apple", s2 = "banana"
+Output: ["banana"]
+*/
+        public void solutia7()
+        {
+
+            string s1 = "this apple is sweet";
+            string s2 = "this apple is sour";
+            string[] result1 = functii.UncommonFromSentences(s1, s2);
+            Console.WriteLine(string.Join(", ", result1));
+        }
+
+        /*Alice and Bob have a different total number of candies. You are given two integer arrays aliceSizes and bobSizes where aliceSizes[i] is the number of candies of the ith box of candy that Alice has and bobSizes[j] is the number of candies of the jth box of candy that Bob has.
+
+Since they are friends, they would like to exchange one candy box each so that after the exchange, they both have the same total amount of candy. The total amount of candy a person has is the sum of the number of candies in each box they have.
+
+Return an integer array answer where answer[0] is the number of candies in the box that Alice must exchange, and answer[1] is the number of candies in the box that Bob must exchange. If there are multiple answers, you may return any one of them. It is guaranteed that at least one answer exists.
+
+
+
+Example 1:
+
+Input: aliceSizes = [1,1], bobSizes = [2,2]
+Output: [1,2]
+Example 2:
+
+Input: aliceSizes = [1,2], bobSizes = [2,3]
+Output: [1,2]
+Example 3:
+
+Input: aliceSizes = [2], bobSizes = [1,3]
+Output: [2,3]
+*/
+        public void solutia8()
+        {
+
+            int[] alice1 = { 1, 1 };
+            int[] bob1 = { 2, 2 };
+            int[] result1 = functii.FairCandySwap(alice1, bob1);
+            Console.WriteLine(string.Join(", ", result1));
+        }
+
+        /*You are given an n x n grid where you have placed some 1 x 1 x 1 cubes. Each value v = grid[i][j] represents a tower of v cubes placed on top of cell (i, j).
+
+After placing these cubes, you have decided to glue any directly adjacent cubes to each other, forming several irregular 3D shapes.
+
+Return the total surface area of the resulting shapes.
+
+Note: The bottom face of each shape counts toward its surface area.
+
+
+
+Example 1:
+
+
+Input: grid = [[1,2],[3,4]]
+Output: 34
+Example 2:
+
+
+Input: grid = [[1,1,1],[1,0,1],[1,1,1]]
+Output: 32
+Example 3:
+
+
+Input: grid = [[2,2,2],[2,1,2],[2,2,2]]
+Output: 46
+*/
+        public void solutia9()
+        {
+
+            int[][] grid1 = {
+            new int[] {1, 2},
+            new int[] {3, 4}
+        };
+            Console.WriteLine(functii.SurfaceArea(grid1));
+        }
+
+        /*An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+
+
+Example 1:
+
+Input: nums = [1,2,2,3]
+Output: true
+Example 2:
+
+Input: nums = [6,5,4,4]
+Output: true
+Example 3:
+
+Input: nums = [1,3,2]
+Output: false
+*/
+        public void solutia10()
+        {
+
+            int[] nums1 = { 1, 2, 2, 3 };
+            Console.WriteLine(functii.IsMonotonic(nums1));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
