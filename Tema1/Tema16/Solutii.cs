0a8a4b4f84fcd9ebf05359da50f088a58693dc25
sandrugabriel <sandru11.gabriel@gmail.com>
2024-07-22 15:34:02 +0300

    done

---
+++ Tema1/Tema16/Solutii.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema16
+{
+    public class Solutii
+    {
+        Functii functii = new Functii();
+
+        /*Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+Example 2:
+
+
+Input: root = [5,1,7]
+Output: [1,null,5,null,7]
+*/
+        public void solutia1()
+        {
+            TreeNode example1 = new TreeNode(5,
+         new TreeNode(3,
+             new TreeNode(2, new TreeNode(1)),
+             new TreeNode(4)),
+         new TreeNode(6, null,
+             new TreeNode(8,
+                 new TreeNode(7),
+                 new TreeNode(9))));
+
+            TreeNode result1 = functii.IncreasingBST(example1);
+            PrintTree(result1);
+        }
+        static void PrintTree(TreeNode root)
+        {
+            TreeNode current = root;
+            while (current != null)
+            {
+                Console.Write(current.val + " ");
+                current = current.right;
+            }
+            Console.WriteLine();
+        }
+
+        /*Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+
+
+
+Example 1:
+
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+Example 2:
+
+Input: nums = [0]
+Output: [0]*/
+        public void solutia2()
+        {
+            int[] example1 = { 3, 1, 2, 4 };
+            int[] result1 = functii.SortArrayByParity(example1);
+            PrintArray(result1);
+        }  
+        static void PrintArray(int[] arr)
+            {
+                Console.WriteLine("[" + string.Join(",", arr) + "]");
+            }
+
+        /*You are given an integer array nums and an integer k.
+
+In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most once for each index i.
+
+The score of nums is the difference between the maximum and minimum elements in nums.
+
+Return the minimum score of nums after applying the mentioned operation at most once for each index in it.
+
+
+
+Example 1:
+
+Input: nums = [1], k = 0
+Output: 0
+Explanation: The score is max(nums) - min(nums) = 1 - 1 = 0.
+Example 2:
+
+Input: nums = [0,10], k = 2
+Output: 6
+Explanation: Change nums to be [2, 8]. The score is max(nums) - min(nums) = 8 - 2 = 6.
+Example 3:
+
+Input: nums = [1,3,6], k = 3
+Output: 0
+Explanation: Change nums to be [4, 4, 4]. The score is max(nums) - min(nums) = 4 - 4 = 0.*/
+        public void solutia3()
+        {
+
+            int[] example1 = { 1 };
+            Console.WriteLine(functii.SmallestRangeI(example1, 0));
+        }
+
+        /*You are given an integer array deck where deck[i] represents the number written on the ith card.
+
+Partition the cards into one or more groups such that:
+
+Each group has exactly x cards where x > 1, and
+All the cards in one group have the same integer written on them.
+Return true if such partition is possible, or false otherwise.
+
+
+
+Example 1:
+
+Input: deck = [1,2,3,4,4,3,2,1]
+Output: true
+Explanation: Possible partition [1,1],[2,2],[3,3],[4,4].
+Example 2:
+
+Input: deck = [1,1,1,2,2,2,3,3]
+Output: false
+Explanation: No possible partition.
+*/
+        public void solutia4()
+        {
+
+            int[] example1 = { 1, 2, 3, 4, 4, 3, 2, 1 };
+            Console.WriteLine(functii.HasGroupsSizeX(example1));
+        }
+
+        /*Given a string s, reverse the string according to the following rules:
+
+All the characters that are not English letters remain in the same position.
+All the English letters (lowercase or uppercase) should be reversed.
+Return s after reversing it.
+
+
+
+Example 1:
+
+Input: s = "ab-cd"
+Output: "dc-ba"
+Example 2:
+
+Input: s = "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+
+Input: s = "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+*/
+        public void solutia5()
+        {
+
+            string example1 = "ab-cd";
+            Console.WriteLine(functii.ReverseOnlyLetters(example1));
+        }
+
+        /*Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+Return any answer array that satisfies this condition.
+
+
+
+Example 1:
+
+Input: nums = [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+Example 2:
+
+Input: nums = [2,3]
+Output: [2,3]*/
+        public void solutia6()
+        {
+
+            int[] example1 = { 4, 2, 5, 7 };
+            Console.WriteLine(string.Join(", ", functii.SortArrayByParityII(example1)));
+        }
+
+        /*Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long pressed, and the character will be typed 1 or more times.
+
+You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed.
+
+
+
+Example 1:
+
+Input: name = "alex", typed = "aaleex"
+Output: true
+Explanation: 'a' and 'e' in 'alex' were long pressed.
+Example 2:
+
+Input: name = "saeed", typed = "ssaaedd"
+Output: false
+Explanation: 'e' must have been pressed twice, but it was not in the typed output.*/
+        public void solutia7()
+        {
+
+            string name1 = "alex";
+            string typed1 = "aaleex";
+            Console.WriteLine(functii.IsLongPressedName(name1, typed1));
+        }
+
+        /*Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+
+For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
+If you add periods '.' between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name. Note that this rule does not apply to domain names.
+
+For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.
+If you add a plus '+' in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered. Note that this rule does not apply to domain names.
+
+For example, "m.y+name@email.com" will be forwarded to "my@email.com".
+It is possible to use both of these rules at the same time.
+
+Given an array of strings emails where we send one email to each emails[i], return the number of different addresses that actually receive mails.
+
+
+
+Example 1:
+
+Input: emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+Output: 2
+Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails.
+Example 2:
+
+Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+Output: 3*/
+        public void solutia8()
+        {
+
+            string[] example1 = {
+            "test.email+alex@leetcode.com",
+            "test.e.mail+bob.cathy@leetcode.com",
+            "testemail+david@lee.tcode.com"
+        };
+            Console.WriteLine(functii.NumUniqueEmails(example1));
+        }
+
+        /*You have a RecentCounter class which counts the number of recent requests within a certain time frame.
+
+Implement the RecentCounter class:
+
+RecentCounter() Initializes the counter with zero recent requests.
+int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of requests that has happened in the past 3000 milliseconds (including the new request). Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
+It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
+
+
+
+Example 1:
+
+Input
+["RecentCounter", "ping", "ping", "ping", "ping"]
+[[], [1], [100], [3001], [3002]]
+Output
+[null, 1, 2, 3, 3]
+
+Explanation
+RecentCounter recentCounter = new RecentCounter();
+recentCounter.ping(1);     // requests = [1], range is [-2999,1], return 1
+recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return 2
+recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], return 3
+recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
+*/
+        public void solutia9()
+        {
+
+            RecentCounter recentCounter = new RecentCounter();
+            Console.WriteLine(recentCounter.Ping(1));
+        }
+
+        /*Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+
+
+Example 1:
+
+
+Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+Output: 32
+Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+Example 2:
+
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+Output: 23
+Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
+*/
+        public void solutia10()
+        {
+
+            TreeNode root1 = new TreeNode(10,
+                                 new TreeNode(5,
+                                     new TreeNode(3, new TreeNode(1), null),
+                                     new TreeNode(7)),
+                                 new TreeNode(15, null, new TreeNode(18)));
+
+            Console.WriteLine(functii.RangeSumBST(root1, 7, 15));
+        }
+
+    }
+
+}
